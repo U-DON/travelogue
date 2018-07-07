ab8221d32cec057a63ebe1dabc56e168c2516abb
Eugene Doan <doan.eugene@gmail.com>
2018-07-07 09:18:06 -0400

    Initial front-end setup with minor directory restructure and Docker config.

---
+++ client/src/js/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../css/App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">Welcome to React</h1>
+        </header>
+        <section className="app-intro">
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
